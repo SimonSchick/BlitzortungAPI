f7aad0dd8e6709eb973d6c22f823ef5efc4669ca
Simon Schick <demwizzy@gmail.com>
2019-02-19 19:40:32 -0800

    chore: updates

--- src/Client.ts
+++ src/Client.ts
@@ -3,228 +3,218 @@ import * as WebSocket from 'ws';
 import { EventEmitter } from 'events';
 
 export interface SocketFactory {
-    make(url: string): WebSocket;
+  make(url: string): WebSocket;
 }
 
 export interface GeoLocation {
-    latitude: number;
-    longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
 export interface GeoArea {
-    from: GeoLocation;
-    to: GeoLocation;
+  from: GeoLocation;
+  to: GeoLocation;
 }
 
 export const enum Polarity {
-    Negative,
-    Positive,
+  Negative,
+  Positive,
 }
 
 interface RawLocation {
-    lat: number;
-    lon: number;
-    /**
-     * altitude in meters.
-     */
-    alt: number;
+  lat: number;
+  lon: number;
+  /**
+    * altitude in meters.
+    */
+  alt: number;
 }
 
 interface RawDetector extends RawLocation {
-    /**
-     * Station ID.
-     */
-    sta: string;
-    /**
-     * Bitflag.
-     */
-    status: number;
-    /**
-     * Delay in nanoseconds.
-     */
-    time: number
+  /**
+    * Station ID.
+    */
+  sta: string;
+  /**
+    * Bitflag.
+    */
+  status: number;
+  /**
+    * Delay in nanoseconds.
+    */
+  time: number
 }
 
 interface RawStrike extends RawLocation {
-    /**
-     * Unix timestamp in nanoseconds
-     */
-    time: number;
-    /**
-     * Polarity, polarity, -1 or +1
-     */
-    pol: number;
-    /**
-     * Deviation.
-     */
-    mds: number;
-    /**
-     * Max circular gap
-     */
-    mcg: number;
-    /**
-     * List of detectors.
-     */
-    sig: RawDetector[];
-    /**
-     * In seconds.
-     */
-    delay: number;
+  /**
+    * Unix timestamp in nanoseconds
+    */
+  time: number;
+  /**
+    * Polarity, polarity, -1 or +1
+    */
+  pol: number;
+  /**
+    * Deviation.
+    */
+  mds: number;
+  /**
+    * Max circular gap
+    */
+  mcg: number;
+  /**
+    * List of detectors.
+    */
+  sig: RawDetector[];
+  /**
+    * In seconds.
+    */
+  delay: number;
 }
 
 export interface Location extends GeoLocation {
-    altitude: number;
+  altitude: number;
 }
 
 export interface Detector {
-    /**
-     * ID.
-     */
-    id: string;
-    location: Location;
-    /**
-     * Unknown.
-     */
-    status: number;
-    /**
-     * Delay in nanoseconds.
-     */
-    delay: number
+  /**
+  * ID.
+  */
+  id: string;
+  location: Location;
+  /**
+  * Unknown.
+  */
+  status: number;
+  /**
+  * Delay in nanoseconds.
+  */
+  delay: number
 }
 
 export interface Strike {
-    location: Location;
-    time: Date;
-    detectors: Detector[];
-    delay: number;
-    deviation: number;
-    polarity: Polarity;
-    maxDeviation: number;
-    maxCircularGap: number;
+  location: Location;
+  time: Date;
+  detectors: Detector[];
+  delay: number;
+  deviation: number;
+  polarity: Polarity;
+  maxDeviation: number;
+  maxCircularGap: number;
 }
 
 export class NotConnectedError extends Error {
-    constructor (public readonly client: Client) {
-        super('Client not connected');
-    }
+  constructor(public readonly client: Client) {
+    super('Client not connected');
+  }
+}
+
+export interface Client {
+  on(event: 'data', cb: (strike: Strike) => void): this;
+  on(event: 'connect', cb: (socket: WebSocket) => void): this;
+  on(event: 'error', cb: (error: Error) => void): this;
+
+  once(event: 'data', cb: (strike: Strike) => void): this;
+  once(event: 'connect', cb: (socket: WebSocket) => void): this;
+  once(event: 'error', cb: (error: Error) => void): this;
+
+  addListener(event: 'data', cb: (strike: Strike) => void): this;
+  addListener(event: 'connect', cb: (socket: WebSocket) => void): this;
+  addListener(event: 'error', cb: (error: Error) => void): this;
 }
 
 export class Client extends EventEmitter {
-    private socket: WebSocket | undefined;
+  private socket: WebSocket | undefined;
 
-    constructor (private socketFactory: SocketFactory) {
-        super();
-    }
+  constructor(private socketFactory: SocketFactory) {
+    super();
+  }
 
-    public on(event: 'data', cb: (strike: Strike) => void): this;
-    public on(event: 'connect', cb: (socket: WebSocket) => void): this;
-    public on(event: 'error', cb: (error: Error) => void): this;
-    public on (event: string, cb: (strike: any) => void): this {
-        super.on(event, cb);
-        return this;
-    }
+  public getSocket(): WebSocket | undefined {
+    return this.socket;
+  }
 
-    public once(event: 'data', cb: (strike: Strike) => void): this;
-    public once(event: 'connect', cb: (socket: WebSocket) => void): this;
-    public once(event: 'error', cb: (error: Error) => void): this;
-    public once (event: string, cb: (strike: any) => void): this {
-        super.once(event, cb);
-        return this;
-    }
+  /**
+  * Connects to the remote API.
+  */
+  public connect() {
+    const socket = this.socket = this.socketFactory.make('ws://ws.blitzortung.org:8060/');
+    socket.on('message', (rawData: string) => {
+      this.emit('data', this.buildStrikeData(JSON.parse(rawData)));
+    });
+    socket.on('open', () => {
+      this.emit('connect', socket);
+    });
+    socket.on('error', err => this.emit('error', err));
+  }
 
-    public addListener(event: 'data', cb: (strike: Strike) => void): this;
-    public addListener(event: 'connect', cb: (socket: WebSocket) => void): this;
-    public addListener(event: 'error', cb: (error: Error) => void): this;
-    public addListener (event: string, cb: (strike: any) => void): this {
-        super.addListener(event, cb);
-        return this;
+  /**
+    * Closes the connection to the remote API and detaches all listeners.
+    */
+  public close() {
+    if (!this.socket) {
+      throw new NotConnectedError(this);
     }
+    this.socket.close();
+    this.removeAllListeners();
+  }
 
-    public getSocket (): WebSocket | undefined {
-        return this.socket;
+  /**
+    * Sets the geo area the API should send data for.
+    * Please note that this does not affect the incoming data immediately.
+    */
+  public setArea(area: GeoArea) {
+    if (!this.socket) {
+      throw new NotConnectedError(this);
     }
+    this.sendJSON({
+      west: area.from.longitude,
+      east: area.to.longitude,
+      north: area.from.latitude,
+      south: area.to.latitude
+    });
+  }
 
-    /**
-     * Connects to the remote API.
-     */
-    public connect () {
-        const socket = this.socket = this.socketFactory.make('ws://ws.blitzortung.org:8060/');
-        socket.on('message', (rawData: string) => {
-            this.emit('data', this.buildStrikeData(JSON.parse(rawData)));
-        });
-        socket.on('open', () => {
-            this.emit('connect', socket);
-        });
-        socket.on('error', err => this.emit('error', err));
+  /**
+    * Requested the API include/omit any detector data.
+    * Please note that this does not affect the incoming data immediately.
+    */
+  public setIncludeDetectors(include: boolean) {
+    if (!this.socket) {
+      throw new Error('Socket not connected');
     }
+    this.sendJSON({
+      sig: include,
+    });
+  }
 
-    /**
-     * Closes the connection to the remote API and detaches all listeners.
-     */
-    public close () {
-        if (!this.socket) {
-            throw new NotConnectedError(this);
-        }
-        this.socket.close();
-        this.removeAllListeners();
-    }
+  private processRawLocation(location: { lat: number; lon: number; alt: number }): Location {
+    return {
+      latitude: location.lat,
+      longitude: location.lon,
+      altitude: location.alt,
+    };
+  }
 
-    /**
-     * Sets the geo area the API should send data for.
-     * Please note that this does not affect the incoming data immediately.
-     */
-    public setArea(area: GeoArea) {
-        if (!this.socket) {
-            throw new NotConnectedError(this);
-        }
-        this.sendJSON({
-            west: area.from.longitude,
-            east: area.to.longitude,
-            north: area.from.latitude,
-            south: area.to.latitude
-        });
+  private buildStrikeData(strike: RawStrike): Strike {
+    return {
+      location: this.processRawLocation(strike),
+      deviation: strike.mds,
+      delay: strike.delay,
+      time: new Date(Math.floor(strike.time / 1e6)),
+      detectors: strike.sig.map(rawDec => ({
+        id: rawDec.sta,
+        location: this.processRawLocation(rawDec),
+        status: rawDec.status,
+        delay: rawDec.time,
+      })),
+      polarity: strike.pol > 0 ? Polarity.Positive : Polarity.Negative,
+      maxDeviation: strike.mds,
+      maxCircularGap: strike.mcg,
     }
+  }
 
-    /**
-     * Requested the API include/omit any detector data.
-     * Please note that this does not affect the incoming data immediately.
-     */
-    public setIncludeDetectors (include: boolean) {
-        if (!this.socket) {
-            throw new Error('Socket not connected');
-        }
-        this.sendJSON({
-            sig: include,
-        });
-    }
-
-    private processRawLocation (location: { lat: number; lon: number; alt: number }): Location {
-        return {
-            latitude: location.lat,
-            longitude: location.lon,
-            altitude: location.alt,
-        };
-    }
-
-    private buildStrikeData (strike: RawStrike): Strike {
-        return {
-            location: this.processRawLocation(strike),
-            deviation: strike.mds,
-            delay: strike.delay,
-            time: new Date(Math.floor(strike.time / 1e6)),
-            detectors: strike.sig.map(rawDec=> ({
-                id: rawDec.sta,
-                location: this.processRawLocation(rawDec),
-                status: rawDec.status,
-                delay: rawDec.time,
-            })),
-            polarity: strike.pol > 0 ? Polarity.Positive : Polarity.Negative,
-            maxDeviation: strike.mds,
-            maxCircularGap: strike.mcg,
-        }
-    }
-
-    private sendJSON(data: any) {
-        this.socket!.send(JSON.stringify(data));
-    }
+  private sendJSON(data: any) {
+    this.socket!.send(JSON.stringify(data));
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './Client';
+export { locations } from './locations';
